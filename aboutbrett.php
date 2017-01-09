752879ded3b3566537d0678fffb9d0b2857397ab
Brett Gilbert <bgilb13@gmail.com>
2017-01-09 16:28:29 -0700

    Added gitignore for Deep Dive projects

---
+++ aboutbrett.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>About Brett</title>
+	</head>
+	<body>
+		<header style="color:blue; font-size:20px">Info</header>
+		<p>My name is Brett Gilbert. I am from Albuquerque, New Mexico and am currently learning web development.</p>
+		<header style="color:red; font-size:20px">Hobbies and Pets</header>
+		<p>I enjoy playing disc golf and spending time with my family and girlfriend. I have 2 dogs named Cocoa and Bailey and a Parrotlet named Petrie.</p>
+		<img src="images/discgolf.jpg" alt="Disc Golf">
+	</body>
+</html>
