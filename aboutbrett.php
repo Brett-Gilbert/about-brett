0ef77d411866b5098404162af76b3e53be73667c
Valente Meza <kingracoon64@gmail.com>
2017-01-10 09:48:25 -0700

    I wrote the truth

--- aboutbrett.php
+++ aboutbrett.php
@@ -9,5 +9,10 @@
 		<header style="color:red; font-size:20px">Hobbies and Pets</header>
 		<p>I enjoy playing disc golf and spending time with my family and girlfriend. I have 2 dogs named Cocoa and Bailey and a Parrotlet named Petrie.</p>
 		<img src="images/discgolf.jpg" alt="Disc Golf">
+        After beating everyone at diskgolf in the world. I decided to take it to the final frontier.
+        Space!
+        Unfortunatly space travel is far behind what it should be and as the writter should of noticed my dog could not travel with me,
+        so i decided to do the next best thing.
+        Full stack programming!
 	</body>
 </html>
